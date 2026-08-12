833a6d107ea7a1d8498ee0b8208dd21fb22d27e1
leouix <nsht22sola@gmail.com>
2026-08-12 22:19:27 +0400

    css sphere

--- src/components/DailyPredictionMap.js
+++ src/components/DailyPredictionMap.js
@@ -201,7 +201,7 @@ function PlanetLabel({
 
 export default function DailyPredictionMap({ paths, size, height = size, selectedId = null, onSelect }) {
   const cx = size / 2
-  const cy = height * 0.45
+  const cy = height * 0.45 - 30
 
   const CHART_R = size * CHART_R_F
   const NATAL_R = CHART_R * NATAL_R_F
